b5ba64b4e558eab111860cc7c2cb42991f494a89
Kevin ADOSSOU <111183740+kevinCaris@users.noreply.github.com>
2023-06-02 11:03:09 +0100

    Update 0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -2,7 +2,22 @@
 #include <string.h>
 #include "main.h"
 
-char *_strcat(char *dest, char *src)
-{
-return (strcat(dest,src));
+char* _strcat(char* dest, const char* src) {
+    char* original_dest = dest;  // Sauvegarde du pointeur original de destination
+
+    // Déplacement du pointeur dest à la fin de la chaîne
+    while (*dest != '\0') {
+        dest++;
+    }
+
+    // Copie des caractères de src à dest (y compris le caractère nul de fin)
+    while (*src != '\0') {
+        *dest = *src;
+        dest++;
+        src++;
+    }
+
+    *dest = '\0';  // Ajout du caractère nul de fin à la fin de dest
+
+    return original_dest;
 }
