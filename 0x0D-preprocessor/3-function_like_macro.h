c032295a52f9821dad6a1c3959b0ba3ffeb4c05d
Kevin ADOSSOU <111183740+kevinCaris@users.noreply.github.com>
2023-06-20 08:58:08 +0100

    Add files via upload

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,11 @@
+#ifndef ABS_H
+#define ABS_H
+
+/*
+ * Function-like macro ABS(x) that computes the
+ * absolute value of a number x.
+ */
+
+#define ABS(x) ((x) < 0 ? -(x) : (x))
+
+#endif /* ABS_H */
